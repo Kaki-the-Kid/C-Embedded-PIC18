90774ab91775ec596b3a71bdea1c8992ee92d12a
Karsten Reitan Sørensen <44589560+Kaki-the-Kid@users.noreply.github.com>
2021-12-11 16:30:54 +0100

    Initial commit

---
+++ testTimer2_5465.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=testTimer2_5465.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/testTimer2_5465.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testtimer25465.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testtimer25465.x.tar
---
+++ tyutgjyhgjh.X/mcc_generated_files/pin_manager.h
@@ -0,0 +1,125 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.5
+        Device            :  PIC18F26K22
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.20 and above
+        MPLAB 	          :  MPLAB X 5.40	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set led1 aliases
+#define led1_TRIS                 TRISAbits.TRISA0
+#define led1_LAT                  LATAbits.LATA0
+#define led1_PORT                 PORTAbits.RA0
+#define led1_ANS                  ANSELAbits.ANSA0
+#define led1_SetHigh()            do { LATAbits.LATA0 = 1; } while(0)
+#define led1_SetLow()             do { LATAbits.LATA0 = 0; } while(0)
+#define led1_Toggle()             do { LATAbits.LATA0 = ~LATAbits.LATA0; } while(0)
+#define led1_GetValue()           PORTAbits.RA0
+#define led1_SetDigitalInput()    do { TRISAbits.TRISA0 = 1; } while(0)
+#define led1_SetDigitalOutput()   do { TRISAbits.TRISA0 = 0; } while(0)
+#define led1_SetAnalogMode()      do { ANSELAbits.ANSA0 = 1; } while(0)
+#define led1_SetDigitalMode()     do { ANSELAbits.ANSA0 = 0; } while(0)
+
+// get/set knap aliases
+#define knap_TRIS                 TRISAbits.TRISA1
+#define knap_LAT                  LATAbits.LATA1
+#define knap_PORT                 PORTAbits.RA1
+#define knap_ANS                  ANSELAbits.ANSA1
+#define knap_SetHigh()            do { LATAbits.LATA1 = 1; } while(0)
+#define knap_SetLow()             do { LATAbits.LATA1 = 0; } while(0)
+#define knap_Toggle()             do { LATAbits.LATA1 = ~LATAbits.LATA1; } while(0)
+#define knap_GetValue()           PORTAbits.RA1
+#define knap_SetDigitalInput()    do { TRISAbits.TRISA1 = 1; } while(0)
+#define knap_SetDigitalOutput()   do { TRISAbits.TRISA1 = 0; } while(0)
+#define knap_SetAnalogMode()      do { ANSELAbits.ANSA1 = 1; } while(0)
+#define knap_SetDigitalMode()     do { ANSELAbits.ANSA1 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
