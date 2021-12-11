a31a3c317ed79d09e8964b5ead1c811503cfac86
Karsten Reitan Sørensen <44589560+Kaki-the-Kid@users.noreply.github.com>
2021-12-11 15:17:56 +0100

    Initial commit

---
+++ 01a. Button/main.c
@@ -0,0 +1,40 @@
+/**
+ * @author:     Karsten Reitan Sørensen
+ * @date:       17-10-2020
+ * @brief:      
+ */
+
+#include "mcc_generated_files/mcc.h"
+
+/*
+                         Main application
+ */
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    while (1)
+    {
+        // Add your application code
+    }
+}
+/**
+ End of File
+*/
